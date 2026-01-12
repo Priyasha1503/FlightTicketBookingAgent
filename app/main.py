18680270f17bc1aa49cbef1d473ece323a42b6db
USER <you@example.com>
2026-01-12 16:34:45 +0530

    everything

--- app/main.py
+++ app/main.py
@@ -1,11 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from app.database import SessionLocal
+from app import models, schemas
+from app.routes import user,flight
+app = FastAPI(title="Flight Ticket Booking Agent")
 
-app = FastAPI()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
-def root():
-    return {"msg": "root works"}
+def health_check():
+    return {"status": "Agent is running"}
 
-@app.get("/test")
-def test():
-    return {"msg": "test works"}
+@app.post("/preferences/")
+def create_preference(
+    preference: schemas.TicketPreferenceCreate,
+    db: Session = Depends(get_db)
+):
+    db_pref = models.TicketPreference(**preference.dict())
+    db.add(db_pref)
+    db.commit()
+    db.refresh(db_pref)
+    return db_pref
